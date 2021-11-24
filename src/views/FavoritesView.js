56529b183d5f4d96ae5874b1c90f36ac0065ce1a
CheeseCheesson <kira7999@gmail.com>
2021-11-24 12:12:24 +0300

    add FavoritesView add save add remove functions to the filmsService.js

--- src/views/FavoritesView.js
+++ src/views/FavoritesView.js
@@ -1,33 +1,39 @@
 import View from "./View";
-import { Routes } from "../core/constans/routes"
+import { Routes } from "../core/constans/routes";
+import { renderFilmComponent } from "../core/components/filmComponent";
 
 export class FavoritesView extends View {
   static #Text = {
-    title: 'Your Favorites filme',
-    seeAllFilmsButtonText: 'See All Films'
-  }
+    title: "Your Favorites filme",
+    seeAllFilmsButtonText: "See All Films",
+  };
   render(favoriteFilmModels = []) {
-    const container = document.createElement('div');
-    container.className = 'favorite-container';
-    container.textContent = 'Favorites filme'
+    const container = document.createElement("div");
+    container.className = "favorite-container";
+    container.textContent = FavoritesView.#Text.title;
 
-    const titleHTML = document.createElement('h1')
-    titleHTML.className = 'film-cards-container__title'
-    titleHTML.textContent = FavoritesView.#Text.title
+    const titleHTML = document.createElement("h1");
+    titleHTML.className = "film-cards-container__title";
+    titleHTML.textContent = FavoritesView.#Text.title;
 
-    const linksBlock = document.createElement('div')
-    linksBlock.className = 'film-cards-container__links-block'
-    const allFilmsLink = document.createElement('a')
-    allFilmsLink.className = 'film-cards-container__link-button'
-    allFilmsLink.textContent = FavoritesView.#Text.seeAllFilmsButtonText
-    allFilmsLink.href = `#${Routes.Main}`
-    linksBlock.append(allFilmsLink)
+    const linksBlock = document.createElement("div");
+    linksBlock.className = "film-cards-container__links-block";
+    const allFilmsLink = document.createElement("a");
+    allFilmsLink.className = "link-button film-cards-container__link-button";
+    allFilmsLink.textContent = FavoritesView.#Text.seeAllFilmsButtonText;
+    allFilmsLink.href = `#${Routes.Main}`;
+    linksBlock.append(allFilmsLink);
 
-    //! для всех избранных фильмов. 
-    const filmsContainer = document.createElement('div')
-    filmsContainer.className = 'film-cards-container'
+    //! для всех избранных фильмов.
+    const filmsContainer = document.createElement("div");
+    filmsContainer.className = "film-cards-container";
 
-    container.append(title.append(document.createElement('div').textContent='Привет'), linksBlock)
+    favoriteFilmModels.forEach((filmModel) => {
+      const filmHTML = renderFilmComponent({ filmModel });
+      filmsContainer.append(filmHTML);
+    });
+
+    container.append(titleHTML, linksBlock, filmsContainer);
 
     this.getRoot().append(container);
   }
--- src/views/FilmsView.js
+++ src/views/FilmsView.js
@@ -1,66 +1,25 @@
 import View from "./View";
 import { Routes } from "../core/constans/routes"
-import InFavoritesImage from '../assets/icons/heart-outlined.png'
-import notIsFavoritesImage from '../assets/icons/heart.png'
-import filmDefaultImg from '../assets/image_default/film_default.jpg'
+import {renderFilmComponent} from '../core/components/filmComponent'
+
 
 export class FilmsView extends View {
   static #Text = {
     SeeFavoritFilms: 'See favorite films',
     allFilms: 'All Filmes'
   }
-
   #renderSeeFavoritButton(){
     const containerSeeButton = document.createElement('div')
-    containerSeeButton.className = 'link-button film-cards-container film-card'
+    containerSeeButton.className = 'link-button film-cards-container__links-block'
     
     const seeFavoritesButton = document.createElement('a')
-    seeFavoritesButton.className = 'film-cards-container__link-button'
+    seeFavoritesButton.className = 'link-button film-cards-container__link-button'
     seeFavoritesButton.textContent = FilmsView.#Text.SeeFavoritFilms
     seeFavoritesButton.href = `#${Routes.Favorites}`
     containerSeeButton.append(seeFavoritesButton)
 
     return containerSeeButton
   }
-
-  #renderFilmHTML(filmModel){
-    const container = document.createElement('div')
-    container.className = 'film-card'
-
-    const titleHTML = document.createElement('span')
-    titleHTML.className = 'film-card__title'
-    titleHTML.textContent = filmModel.getTitle()
-
-    const imageHTML = document.createElement('img')
-    if( filmModel.getPoster() === 'N/A'){
-      imageHTML.src = filmDefaultImg
-    } else {
-      imageHTML.src = filmModel.getPoster()
-    }
-    
-    imageHTML.className = 'film-card__poster'
-    imageHTML.alt = filmModel.getTitle()
-
-    const yearHTML = document.createElement('span')
-    yearHTML.className = 'film-card__year'
-    yearHTML.textContent = filmModel.getYear()
-
-    const actionButton = document.createElement('button')
-    actionButton.className = 'film-card__button'
-    const actionButtonImg = document.createElement('img')
-    actionButtonImg.className = 'film-card__button-img'
-    if(filmModel.getIsFavorit()){
-      actionButtonImg.src = InFavoritesImage
-    } else {
-      actionButtonImg.src = notIsFavoritesImage
-    }
-    actionButton.append(actionButtonImg)
-
-    container.append(titleHTML, imageHTML, yearHTML, actionButton)
-  
-    return container
-  }
-
   render(filmModels = []) {
     const container = document.createElement('div');
     container.className = 'films-container'
@@ -76,14 +35,10 @@ export class FilmsView extends View {
     
     //* отрисовка фильмов 
     filmModels.forEach(filmModel => {
-      const filmHTML = this.#renderFilmHTML(filmModel)
+      const filmHTML = renderFilmComponent({filmModel: filmModel})
       filmsContainer.append(filmHTML)
     })
-
     container.append(titleHTML, containerSeeButtonItem, filmsContainer)
-
     this.getRoot().append(container);
-
-  
   }
 }
